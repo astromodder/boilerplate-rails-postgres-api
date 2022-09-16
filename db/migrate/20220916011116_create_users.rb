f6838e39241efd8ea8d9f65732e5dcade0ccaa46
astromodder <22250203+astromodder@users.noreply.github.com>
2022-09-15 21:15:54 -0400

    first stab at my preferences

---
+++ db/migrate/20220916011116_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users, id: :uuid, default: -> { "gen_random_uuid()" } |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
